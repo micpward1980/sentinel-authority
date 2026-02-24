5034b76fce27811f94d55b23b33cfa6efd7bef19
Mike Ward <info@sentinelauthority.org>
2026-02-23 21:06:31 -0600

    Purge all .DS_Store files

--- website/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~/Downloads/sentinel-authority/website
-vercel deploy --prod --yes
-read -p "Paste the Production URL: " URL
-vercel alias "$URL" www.sentinelauthority.org
-vercel alias "$URL" sentinelauthority.org
-echo "Aliases set."
